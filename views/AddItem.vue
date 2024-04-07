81969c0349cb062b1df377449d865411f0bf5006
leesheng2299 <102453893+leesheng2299@users.noreply.github.com>
2024-04-08 02:19:11 +0800

    Add files via upload

---
+++ views/AddItem.vue
@@ -0,0 +1,45 @@
+<script>
+import Add from "@/components/Add.vue";
+import LogOut from "@/components/LogOut.vue";
+import NavBar from "@/components/NavBar.vue";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export default {
+  name: "AddItem",
+  components: {
+    Add,
+    LogOut,
+    NavBar, 
+  },
+  data() {
+    return {
+      refreshComp: 0,
+      user: false,
+    };
+  },
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user;
+      }
+    });
+  },
+
+  methods: {
+    change() {
+      this.refreshComp += 1;
+    },
+  },
+};
+</script>
+
+<template>
+  <div style="text-align: center" v-if="user">
+    <NavBar/>
+    <h3>Add Item</h3>
+    <Add @added="change" />
+    <LogOut />
+    <br /><br />
+  </div>
+</template>
