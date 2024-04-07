929087de02b15e18393d2d5e11140f297478f2df
leesheng2299 <102453893+leesheng2299@users.noreply.github.com>
2024-04-08 02:25:19 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import firebaseApp from "./firebase";
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount("#app");
