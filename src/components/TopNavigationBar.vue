6389db2ed3a7c63d6e66f9b6c6ef42bf3390b26c
leesheng2299 <102453893+leesheng2299@users.noreply.github.com>
2024-04-08 02:32:15 +0800

    Add files via upload

--- src/components/TopNavigationBar.vue
+++ src/components/TopNavigationBar.vue
@@ -1,83 +1,65 @@
 <template>
-    <nav class="navbar">
+  <div id="logged" v-if="user">
+    <div id="nav">
       <div class="navbar-brand">
         <img src = '@/assets/onTrackLogi.jpg' alt="OnTrack Logo">
-        
       </div>
-      <ul class="navbar-menu">
-        <li class="navbar-item"><a href="#">Inventory Management</a></li>
-        <li class="navbar-item"><a href="#">Sales DashBoard</a></li>
-        
-        
-      </ul>
-      <div class="navbar-end">
-        <button class="login-button">Login</button>
-        
-      </div>
-    </nav>
-  </template>
-  
-  <script>
-  
-  export default {
-    
-  };
-  </script>
-  
-  <style scoped>
-  .navbar {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #f4e8de;
-    color: black;
-    height:5vh;
-    width: 100vw;
-  }
-  
-  .navbar-brand img {
+      <router-link to="/about">About</router-link> |
+      <router-link to="/inventory">Inventory Management</router-link> |
+      <router-link to="/sales">Sales Dashboard</router-link> |
+      <router-link to="/profile">Profile</router-link> |
+      <router-link to="/addItem">Add Item</router-link>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export default {
+  name: "NavBar",
+
+  data() {
+    return {
+      user: false,
+    };
+  },
+
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user;
+      }
+    });
+  },
+};
+</script>
+
+<style scoped>
+#nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #f4e8de;
+  color: black;
+  height:5vh;
+  width: 100vw;
+}
+
+.navbar-brand img {
     height: 5vh;
     width: 6vw;
   }
-  
-  
-  /*make horizontal navbar-menu */
-  .navbar-menu {
-    list-style: none;
-    display: flex;
-    margin: 0;
-    padding: 0;
-  }
-  
-  /* margin btw navbar-tems */
-  .navbar-item:not(:first-of-type) {
-    margin-left: 1vw;
-  }
-  
-  .navbar-item a {
-    text-decoration: none;
-    color: black;
-    font-weight: bold;
-    transition: color 0.3s;
-  }
 
-  
-  
-  .navbar-item a:hover {
-    color: #555;
-  }
-  
-  .login-button {
-    background: none;
-    color: black;
-    font-weight: bold;
-    padding: 1vh 2vw;
-    cursor: pointer;
-    border: none;
-  }
-  
-  .login-button:hover {
-    background-color: white;
-  }
-  </style>
-  
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #6366f1;
+}
+</style>
+
