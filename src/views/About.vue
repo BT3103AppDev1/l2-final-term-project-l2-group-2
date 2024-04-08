4f415d07b7e3ff6fe82ca3d605a2f602a6cd2603
leesheng2299 <102453893+leesheng2299@users.noreply.github.com>
2024-04-08 23:35:28 +0800

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="text-align: center" v-if="user">
+    <NavBar/>
+    <h3>This is the About Page</h3>
+    <h2>Welcome to OnTrack</h2>
+    <h3>
+      OnTrack is a real-time inventory management system for you to keep track of all product movements.
+    </h3>
+    <LogOut /> <br /><br />
+  </div>
+</template>
+
+<script>
+import LogOut from "@/components/LogOut.vue";
+import NavBar from "@/components/TopNavigationBar.vue";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+export default {
+  name: "About",
+  components: {
+    LogOut,
+    NavBar,
+  },
+  data() {
+    return {
+      user: false,
+    };
+  },
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user;
+      }
+    });
+  },
+};
+</script>
