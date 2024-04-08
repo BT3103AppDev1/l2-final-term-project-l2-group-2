bc55b2f21a15cced5cc5bc21388315eb11bd4c6a
joshhlim <joshena2606@gmail.com>
2024-04-09 02:40:55 +0800

    Added tables to sales dashboard
    added tables with dummy data for top sales items and worst sales items

--- src/views/Sales.vue
+++ src/views/Sales.vue
@@ -1,32 +1,41 @@
 <template>
+  <div>
     <div style="text-align: center" v-if="user">
       <NavBar />
     </div>
+    <salestable />
+    <salestablebad />
     <LogOut />
-  </template>
-  
-  <script>
-  import LogOut from "@/components/LogOut.vue";
-  import NavBar from "@/components/TopNavigationBar.vue";
-  import { getAuth, onAuthStateChanged } from "firebase/auth";
-  export default {
-    name: "Profile",
-    components: {
-      LogOut,
-      NavBar,
-    },
-    data() {
-      return {
-        user: false,
-      };
-    },
-    mounted() {
-      const auth = getAuth();
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          this.user = user;
-        }
-      });
-    },
-  };
-  </script>
+  </div>
+</template>
+
+<script>
+import LogOut from "@/components/LogOut.vue";
+import NavBar from "@/components/TopNavigationBar.vue";
+import salestable from "@/components/SalesTable.vue";
+import salestablebad from "@/components/SalesTablesBad.vue";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export default {
+  name: "Profile",
+  components: {
+    salestablebad,
+    salestable,
+    LogOut,
+    NavBar,
+  },
+  data() {
+    return {
+      user: false,
+    };
+  },
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user;
+      }
+    });
+  }
+};
+</script>
